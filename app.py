d954debb98ec5ecd0c797e6729a89a1f43ba3ee5
shifumix <dev@shifumix.com>
2019-01-27 11:12:56 +0100

    version avec fichier

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask,jsonify,Response
 from googlesearch import search,hits
 import pandas as pd
+from urllib.parse import urlparse
 
 app = Flask(__name__)
 
@@ -10,19 +11,35 @@ def hello_world():
     return 'Hello World!'
 
 
-#test : http://localhost:5000/search/Michelin
+def urltodata(url:str):
+    data: pd.DataFrame = None
+    if ".xls" in url: data = pd.read_excel(url)
+    if ".csv" in url: data = pd.read_csv(url, sep=";")
+    return data
+
+
+def extract_domain(url:str):
+    parsed_uri = urlparse(url)
+    result = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
+    return result
+
+#test : http://localhost:5000/search/Michelin/temp.csv
 @app.route('/search/<string:brand>/<string:referentiel>', methods=['GET'])
 def searchforbrand(brand:str,referentiel:str):
     if not referentiel.startswith("http"):
         referentiel="https://raw.githubusercontent.com/f80dev/MaterialityMatrix/master/assets/"+referentiel
 
-    data:pd.DataFrame=None
-    if ".xls" in referentiel:data=pd.read_excel(referentiel)
-    if ".csv" in referentiel:data=pd.read_csv(referentiel)
+    audience=urltodata("https://raw.githubusercontent.com/f80dev/MaterialityMatrix/master/assets/audience.csv")
+    data=urltodata(referentiel)
     if data is None:return Response("Bad format",401)
 
-    for r in data.iterrows():
-        result=hits(brand+" & "+r["google"])
+    for i in range(len(data)):
+        row=data.iloc[i]
+        result=search(brand+" & "+row["query"])
+        classement=[]
+        for r in result:
+            domain=extract_domain(result)
+            classement.push(audience[domain])
 
     return jsonify(result)
 
